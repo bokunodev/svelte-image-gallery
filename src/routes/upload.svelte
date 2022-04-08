ad371d0757c65b1c2b41a5e1d0f0922717474869
bokunocode <bokunocode@gmail.com>
2022-04-08 21:22:04 +0700

    button `+` and `-` now works
    Changes to be committed:
    	modified:   src/routes/upload.svelte

--- src/routes/upload.svelte
+++ src/routes/upload.svelte
@@ -1,4 +1,5 @@
 <script>
+	import Icon from '$lib/components/Icon/Icon.svelte';
 	import { SvelteToast, toast } from '@zerodevx/svelte-toast';
 
 	/** @param {string} err */
@@ -45,6 +46,16 @@
 				notifyError(err);
 			});
 	};
+
+	/**
+	 * NOTE: Only inputs could have `.form-control` class
+	 */
+
+	/** @type {HTMLElement} */
+	let formBody;
+	/** @type {HTMLElement} */
+	let formInputs;
+	let formCount = 0;
 </script>
 
 <SvelteToast />
@@ -54,42 +65,66 @@
 		<div class="col col-12 col-md-8 col-xl-6 mx-auto">
 			<form class="card" on:submit|preventDefault={doSubmit}>
 				<div class="card-header">Upload Image</div>
-				<div class="card-body">
-					<div class="mb-2">
-						<label for="image[]">Image</label>
-						<input
-							class="form-control form-control-sm"
-							type="file"
-							name="image[]"
-							required
-							on:change={(e) => {
-								// @ts-ignore
-								image = e.target.files[0];
-							}}
-						/>
-					</div>
-					<div class="mb-2">
-						<label for="title[]">Title</label>
-						<input
-							class="form-control form-control-sm"
-							type="text"
-							name="title[]"
-							placeholder="Beach Scenery"
-							bind:value={title}
-						/>
-					</div>
-					<div class="mb-2">
-						<label for="caption[]">caption</label>
-						<textarea
-							bind:value={caption}
-							class="form-control form-control-sm"
-							name="caption[]"
-							placeholder="Beautiful day on the beach with friends"
-						/>
+				<div class="card-body" bind:this={formBody}>
+					<div bind:this={formInputs}>
+						<div class="mb-2">
+							<label for="image[]">Image</label>
+							<input class="form-control form-control-sm" type="file" name="image[]" required />
+						</div>
+						<div class="mb-2">
+							<label for="title[]">Title</label>
+							<input
+								class="form-control form-control-sm"
+								type="text"
+								name="title[]"
+								placeholder="Beach Scenery"
+							/>
+						</div>
+						<div class="mb-2">
+							<label for="caption[]">caption</label>
+							<textarea
+								class="form-control form-control-sm"
+								name="caption[]"
+								rows="1"
+								placeholder="Beautiful day on the beach with friends"
+							/>
+						</div>
+						<hr />
 					</div>
 				</div>
 				<div class="card-footer">
 					<button type="submit" class="btn btn-sm btn-primary">Submit</button>
+					<button
+						type="button"
+						class="btn btn-sm btn-outline-primary"
+						on:click={() => {
+							const newInputs = formInputs.cloneNode(true);
+							// reset input all value after clone
+							// @ts-ignore
+							newInputs.querySelectorAll('.form-control').forEach((e) => (e.value = ''));
+							formBody.appendChild(newInputs);
+							formCount++;
+						}}
+					>
+						<Icon>+</Icon>
+					</button>
+					<button
+						type="button"
+						class="btn btn-sm btn-outline-success"
+						on:click={() => {
+							// dont remove element if its the only one
+							// reset their value instead
+							if (formCount < 1) {
+								// @ts-ignore
+								formInputs.querySelectorAll('.form-control').forEach((e) => (e.value = ''));
+								return;
+							}
+							formBody.lastElementChild.remove();
+							formCount--;
+						}}
+					>
+						<Icon>-</Icon>
+					</button>
 				</div>
 			</form>
 		</div>
